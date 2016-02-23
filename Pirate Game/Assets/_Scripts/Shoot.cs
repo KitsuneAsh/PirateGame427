8d898e3643c172a0929410f9a1340e022d596d13
oathkeeper005 <oathkeeper_rmm@hotmail.com>
2016-02-23 15:38:14 -0600

    wtf is going on
    gbifdsadvhpodsavbsv

--- Pirate Game/Assets/_Scripts/Shoot.cs
+++ Pirate Game/Assets/_Scripts/Shoot.cs
@@ -3,22 +3,35 @@ using System.Collections;
 
 public class Shoot : MonoBehaviour {
 
+    [SerializeField]
+    private float RateOfFire = 0.5f;
+    private float NextFire = 0.0f;
+    [SerializeField]
+    private Transform ProjectileSpawn;
 
+
+    [SerializeField]
     private GameObject Ammo;
 
 	// Use this for initialization
 	void Start () {
-	    
+        // InvokeRepeating will invoke a the method in x seconds and then repeat every y seconds
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	    
+        if (Input.GetButton("Fire1") && Time.time > NextFire)
+        {
+            NextFire = Time.time + RateOfFire;
+            //GameObject clone = Instantiate(Projectile, ProjectileSpawn.position, ProjectileSpawn.rotation) as GameObject; //Use This code if you need a reference to the shot you fired?
+            Instantiate(Ammo, transform.position, Quaternion.Euler(0, 0, 0));
+        }
 	}
 
     public void FireProjectile()
     {
-        //Instantiate<>(Ammo);
+        Instantiate(Ammo,gameObject.transform.position, gameObject.transform.rotation);
     }
+
 
 }
